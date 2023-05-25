11d1642e9bee9a912589222cb38093cc741cc10e
HARRY-RAMGARHIA <harpreetharrysm@gmail.com>
2023-05-25 15:02:14 -0600

    added some more information regarding the projrect and planning and started working with tkinter

---
+++ gui_interface_for_project.py
@@ -0,0 +1,4 @@
+# so in tkinter everything is a widget ther are button text and many other widgets including frame widgets 
+#the first ting that is created is the root widget in this case is the main window of the application interface.
+
+from tkinter import * 
